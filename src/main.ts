16739e8f15178e4c6492e983cf608d1cd42818ea
Marco Antônio <ma.souza.junior@gmail.com>
2022-10-16 17:43:51 -0300

    enh: generate data to test services

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
-function main() {
-  console.log("Hello");
+import {
+  computeDaily,
+  makeScoreboardContent,
+} from "#/service/daily-scoreboard";
+
+async function main() {
+  const day = 24 * 60 * 60 * 1000;
+  const list: [string, string][] = [
+    ["marco", "ma"],
+    ["marc123o", "1ma"],
+    ["2marc123o", "4ma"],
+    ["12", "12"],
+    ["22123", "123123"],
+  ];
+
+  await Promise.all(list.map((args) => computeDaily(...args)));
+
+  console.log(await makeScoreboardContent());
 }
 
 main();
