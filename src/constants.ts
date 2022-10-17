de35a698bba71a12e71c2f04ba90ea0022ec49b6
Marco Antônio <ma.souza.junior@gmail.com>
2022-10-17 19:49:54 -0300

    fea: add dotenv loader

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,14 @@
+import * as dotenv from "dotenv";
+
+// Load .env configs
+dotenv.config();
+
+export const {
+  BOT_ID = "",
+  DAILY_CHANNEL = "",
+  DISCORD_TOKEN = "",
+} = process.env;
+
 export const SCOREBOARD_TITLE = "\n**Scoreboard - %date**\n\n";
 
 export const MAX_NAME_LENGTH = 25;
