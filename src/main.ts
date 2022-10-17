85a693da3d62e50e28e0cd1cb17869c01b145e39
Marco Antônio <ma.souza.junior@gmail.com>
2022-10-17 19:59:28 -0300

    fea: add bot counter & commands

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
-import {
-  computeDaily,
-  makeScoreboardContent,
-} from "#/service/daily-scoreboard";
+import { connectToDiscord as startDiscordBot } from "./service/discord";
 
 async function main() {
-  const day = 24 * 60 * 60 * 1000;
-  const list: [string, string][] = [
-    ["marco", "ma"],
-    ["marc123o", "1ma"],
-    ["2marc123o", "4ma"],
-    ["12", "12"],
-    ["22123", "123123"],
-  ];
-
-  await Promise.all(list.map((args) => computeDaily(...args)));
-
-  console.log(await makeScoreboardContent());
+  await startDiscordBot();
+  console.log("bot connected");
 }
 
 main();
---
+++ src/service/discord.ts
@@ -0,0 +1,77 @@
+import { BOT_ID, DAILY_CHANNEL, DISCORD_TOKEN } from "#/constants";
+import { addDailyRecord } from "#/repository/db";
+import {
+  Client,
+  GatewayIntentBits,
+  Interaction,
+  Message,
+  REST,
+  Routes,
+} from "discord.js";
+import { makeScoreboardContent } from "./daily-scoreboard";
+
+const commands = [
+  {
+    name: "scoreboard",
+    description: "Get the scoreboard of the month",
+  },
+];
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+
+const rest = new REST({ version: "10" }).setToken(DISCORD_TOKEN);
+
+export async function connectToDiscord() {
+  // setup bot commands
+  await addCommands();
+
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+  });
+
+  client.on("messageCreate", messageHandler);
+  client.on("messageUpdate", (_prev, next) =>
+    messageHandler(next as Message<boolean>)
+  );
+
+  client.on("interactionCreate", interactionHandler);
+
+  return client.login(DISCORD_TOKEN);
+}
+
+async function addCommands() {
+  console.log("Started refreshing application (/) commands.");
+  await rest.put(Routes.applicationCommands(BOT_ID), { body: commands });
+  console.log("Successfully reloaded application (/) commands.");
+}
+
+async function messageHandler(msg: Message) {
+  const isDailyChannel = msg.channelId === DAILY_CHANNEL;
+  if (!isDailyChannel) return;
+
+  const dailyWords = ["o que eu fiz", "o que vou fazer"];
+  const isDailyMessage = dailyWords.every((word) =>
+    msg.content.toLowerCase().includes(word)
+  );
+  if (!isDailyMessage) return;
+
+  const { username } = msg.author;
+  console.log("adding daily record for:", { username });
+  // TODO: remove name, as we don't need/have it
+  await addDailyRecord({ username, name: username });
+}
+
+async function interactionHandler(interaction: Interaction) {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "scoreboard") {
+    const content = await makeScoreboardContent(10);
+    await interaction.reply(content);
+  }
+}
